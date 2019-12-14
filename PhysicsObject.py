55e321629b21d9dea012f9d29381546a5d9f7def
Sparsha Saha <saha.sparsha@gmail.com>
2019-12-15 04:46:30 +0530

    optimizing performance

--- PhysicsObject.py
+++ PhysicsObject.py
@@ -1,8 +1,9 @@
 class PhysicsObject:
 
-    def __init__(self, x, y, angle, speed, isGravityAffected):
+    def __init__(self, x, y, angle, speed, size, isGravityAffected):
         self.x = x
         self.y = y
         self.angle = angle
         self.speed = speed
         self.isGravityAffected = isGravityAffected
+        self.size = size
--- Test.py
+++ Test.py
@@ -8,18 +8,19 @@ class Particle(PhysicsObject):
 
     def __init__(self):
         self.angle = math.pi/3
-        self.speed = 15
+        self.speed = 5
         self.colour = (0,0,0)
         self.x = 20
         self.y = 20
-        super().__init__(self.x, self.y, self.angle, self.speed, False)
+        super().__init__(self.x, self.y, self.angle, self.speed, 5,  False)
 
     def display(self):
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), 5, 1)
+        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), 5, 5)
     
 
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
+screen = pygame.display.set_mode((600, 600), flags)
 pygame.display.set_caption('Basic Pygame program')
 
 
@@ -27,20 +28,22 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
 
-
 screen.blit(background, (0,0))
 pygame.display.flip()
 par = Particle()
 p = Physics(600, 600)
 p.registerMovingObject(par)
 c = True
+clock = pygame.time.Clock()
 
 while c:
+    clock.tick(80)
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT or event.type == KEYDOWN:
             c = False
     p.performReflectionPhysics()
     screen.blit(background, (0,0))
     par.display()
     pygame.display.flip()
+pygame.quit()
 
