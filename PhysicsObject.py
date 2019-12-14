afd4364a91460d36c96ac4b33cdc7eda16e50f92
Sparsha Saha <saha.sparsha@gmail.com>
2019-12-15 04:11:28 +0530

    PhysicsObject defined

---
+++ PhysicsObject.py
@@ -0,0 +1,7 @@
+class PhysicsObject:
+
+    def __init__(self, angle, speed, isGravityAffected):
+        self.angle = angle
+        self.speed = speed
+        self.isGravityAffected = isGravityAffected
+        
